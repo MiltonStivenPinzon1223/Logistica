32f99ae892d91533ba698fee0a5fd790cf9aa7f2
Jesus Mateo Torres Perez <109240113+contrty@users.noreply.github.com>
2024-09-10 13:16:15 -0500

    routes
    puto

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\AssitenceController;
+use App\Http\Controllers\CertificateController;
+use App\Http\Controllers\CollectionAccountController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\LogisticController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\TypeCertificateController;
 use App\Http\Controllers\TypeClothingController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\LogisticController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +19,12 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('roles', [RoleController::class,'index'])->name('roles.index');
-Route::resource('type/certificates', TypeCertificateController::class)->names('type.certificates');
+Route::resource('assistences', AssistenceController::class)->names('assistences');
+Route::resource('certificates', CertificateController::class)->names('certificates');
+Route::resource('collection/accounts', CollectionAccountController::class)->names('collection.accounts');
 Route::resource('events', EventController::class)->names('events');
 Route::resource('logistics', LogisticController::class)->names('logistics');
+Route::resource('roles', RoleController::class)->names('roles');
+Route::resource('type/certificates', TypeCertificateController::class)->names('type.certificates');
 Route::resource('type/clothings', TypeClothingController::class)->names('type.clothings');
+Route::resource('users', UserController::class)->names('users');
